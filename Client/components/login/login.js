fa794fd04822d33106977e965a28c61178fc5b1b
Mohammad Arsalan Rajput <a4arshi@yahoo.com>
2016-04-13 15:40:41 +0500

    add validation

--- Client/components/login/login.js
+++ Client/components/login/login.js
@@ -7,23 +7,31 @@ angular
         var _self = this;
        _self.user = {};
         _self.login = function(user) {
+             _self.ErrorUserNotExist = "";
+             _self.varificationError = "";
+             _self.error = "";
              if(user !== "") {
                   HttpService.PostApi("/router/signin",user)
                                        .then(function(res){
-                                             if(res.data === "Password does not match"){
-                                                 _self.error = "Password does not match";
-                                            }
-                                            else if(user.rememberMe){
-                                                 $cookieStore.put("cloudToken",res.data._id);
+                                             if(res.data === "Please Varify Email") {
+                                                  _self.varificationError = "Please Varify Your Email Address"
+                                         } else  if(res.data === "Password does not match") {
+                                                  _self.error = "Email Or Password does not match";
+                                             }
+                                             else if(res.data === "User Not Found with this Email Address"){
+                                                  _self.ErrorUserNotExist = "User Not Found with this Email Address"
+                                             }
+                                             else if(user.rememberMe){
+                                                  $cookieStore.put("cloudToken",res.data._id);
 
-                                                 AuthService.isLoggedIn(res.data._id);
-                                                 localStorage.setItem("token",res.data._id);
-                                                 $state.go("dashboard.home");
-                                            }
-                                            else {
-                                                 $cookieStore.put("cloudToken",res.data._id);
-                                                 AuthService.isLoggedIn(res.data._id);
-                                                 $state.go("dashboard.home");
+                                                  AuthService.isLoggedIn(res.data._id);
+                                                  localStorage.setItem("token",res.data._id);
+                                                  $state.go("dashboard.home");
+                                             }
+                                             else {
+                                                  $cookieStore.put("cloudToken",res.data._id);
+                                                  AuthService.isLoggedIn(res.data._id);
+                                                  $state.go("dashboard.home");
                                          }
 
                                        },function(error){
--- Server/model/user/userController.js
+++ Server/model/user/userController.js
@@ -2,6 +2,7 @@ var express = require("express"),
     usermodel_1 = require("./usermodel"),
     postmark = require("postmark"),
     bcrypt = require("bcrypt-nodejs"),
+    md5 = require("md5"),
     postmark = require('postmark');
 var postmarkEmail = new postmark.Client("a96e9e41-6f92-484a-a7f6-c601e82a70fb");
 var SALT_FACTOR = 10;
@@ -9,53 +10,54 @@ var SALT_FACTOR = 10;
 // Main Sign in Function
 function Signin(req, res) {
     usermodel_1.UserModel.findOne({ email: req.body.email }, function(err, success) {
-        if (success) {
-            bcrypt.compare(req.body.password, success.password, function(err, isMatch) {
-                done(err, isMatch);
+        console.log(err , success);
+            if (success) {
+                if(success.status == 1) {
+                    bcrypt.compare(req.body.password, success.password, function(err, isMatch) {
+                        done(err, isMatch);
 
-            });
-            function done(err2, isMatch) {
-                if (isMatch) {
-                    usermodel_1.UserModel.update({ email: req.body.email }, { $set: { timestamp: Date.now() } }, function(err, data) {
-                        var userDetails = {
-                            uid: data._id,
-                            name: data.firstName + " " + data.lastName,
-                            role: data.role
+                    });
+                    function done(err2, isMatch) {
+                        if (isMatch) {
+                            usermodel_1.UserModel.update({ email: req.body.email }, { $set: { timestamp: Date.now() } }, function(err, data) {
+                                res.send(success);
+
+                            })
+                        } else {
+                            res.send("Password does not match");
                         }
-                        res.send(success);
-
-                    })
-                } else {
-                    res.send("Password does not match");
+                    }
                 }
+
             }
-        }
-        else {
-            res.send(err);
-        }
+            else {
+                res.send("User Not Found with this Email Address");
+            }
+
+
     });
 }
 exports.Signin = Signin;
 // User Sign in Function
-function userSignin(req, res) {
-    usermodel_1.AddUserModel.findOne({ email: req.body.email }, function(err, success) {
-        if (success) {
-            bcrypt.compare(req.body.password, success.password, function(err, isMatch) {
-                done(err, isMatch);
-            });
-            function done(err2, isMatch) {
-                isMatch ? res.send({ token: success._id }) : res.send(err);
-            }
-        }
-        else {
-            res.send(err);
-        }
-    });
-}
-exports.userSignin = userSignin;
+// function userSignin(req, res) {
+//     usermodel_1.AddUserModel.findOne({ email: req.body.email }, function(err, success) {
+//         if (success) {
+//             bcrypt.compare(req.body.password, success.password, function(err, isMatch) {
+//                 done(err, isMatch);
+//             });
+//             function done(err2, isMatch) {
+//                 isMatch ? res.send({ token: success._id }) : res.send(err);
+//             }
+//         }
+//         else {
+//             res.send(err);
+//         }
+//     });
+// }
+// exports.userSignin = userSignin;
 // Get All User Function
 function getuser(req, res) {
-    usermodel_1.AddUserModel.find({}, function(err, success) {
+    usermodel_1.UserModel.find({}, function(err, success) {
         if (err) {
             res.send("Error to Find Data");
         }
@@ -172,36 +174,63 @@ function getmember(req, res) {
     });
 }
 exports.getmember = getmember;
+// Get All Memberships Function
+function varification(req, res) {
+    usermodel_1.AddUserModel.findOne({email: req.params.email}, function(err, success) {
+        if (err) {
+            res.send("Error to Find Data");
+        }
+        else {
+            if(success.varifyToken == req.params.id) {
+                success.status = 1;
+                success.save(function(err, success){
+                    if(err){
+                        console.log(err);
+                    }
+                    else {
+                        console.log(success)
+                        res.send(success);
+                    }
+                })
+            };
+
+        }
+    });
+}
+exports.varification = varification;
 // Main Sign up Function
 function Signup(req, res) {
     //token generate..
+    var varifyToken = md5(req.body.email + Date.now());
+    req.body.varifyToken = varifyToken;
+    req.body.status = 0;
     var user = new usermodel_1.UserModel(req.body);
     user.save(function(err, _user) {
         if (err) {
-            res.send(err);
+            res.send("Email is ALready exist");
         }
         else {
             //email verification
-            emailVerification(_user.email, _user.token);
+            emailVerification(_user.email, _user.varifyToken);
             res.send({ message: "Inserted Successfully", token: _user._id });
         }
     });
 }
 exports.Signup = Signup;
-// Add User Function
-function addUser(req, res) {
-    var user = new usermodel_1.AddUserModel(req.body);
-    console.log(req.body);
-    user.save(function(err, data) {
-        if (err) {
-            res.send(err);
-        }
-        else {
-            res.send({ message: "Inserted Successfully", data: data });
-        }
-    });
-}
-exports.addUser = addUser;
+// // Add User Function
+// function addUser(req, res) {
+//     var user = new usermodel_1.AddUserModel(req.body);
+//     console.log(req.body);
+//     user.save(function(err, data) {
+//         if (err) {
+//             res.send(err);
+//         }
+//         else {
+//             res.send({ message: "Inserted Successfully", data: data });
+//         }
+//     });
+// }
+// exports.addUser = addUser;
 // Add Memberships Function
 function addFranchises(req, res) {
     var user = new usermodel_1.FranchiseModel(req.body);
@@ -250,11 +279,12 @@ function sendPasswordRecoveryEmail(user) {
 }
 
 function emailVerification(email, token) {
+    console.log(email , token)
     var template = {
         "To": email,
         "From": "test@holidaycorp.co.za",
         "Subject": 'Please Verify your Email',
-        "HtmlBody": "<h1>Verify Email</h1>, please link on this link for email verification: " + token
+        "HtmlBody": "<h1>Verify Email</h1>, please link on this link for email verification: https://cloudfussion.herukoapp.com/varify/"+ email +  "/" +  token
     };
     postmarkEmail.sendEmail(template,function(err, json) {
         if (err) {
@@ -266,5 +296,3 @@ function emailVerification(email, token) {
         console.log(json);
     });
 }
-  
-  

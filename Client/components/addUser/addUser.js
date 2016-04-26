9e148a927e663d9384ec182fe43c00c38b35f5ed
Mohammad Arsalan <a4arshi@yahoo.com>
2016-04-26 10:55:38 +0500

    add roles with access read and write rules

--- Client/components/addUser/addUser.js
+++ Client/components/addUser/addUser.js
@@ -11,7 +11,23 @@ function AddUserController(mainRef, DataService, MessageService, AuthService,
   var _self = this;
   _self.error = "";
   var mainRef = new Firebase("https://cloudfusionv2.firebaseio.com/");
-  // _self.AllFranchiseName = DataService.Onefranchise();
+  _self.selectedRoles = [];
+  //Get all Roles
+  _self.user = {}
+  _self.Roles = DataService.allRole();
+
+  _self.addSelectedRole = function(role) {
+      console.log(role)
+      _self.selectedRoles.push({
+        role: role,
+        noAccess: false,
+        readOnly: true,
+        write: false
+
+      })
+    }
+    // Get All Franchise Name
+
   MessageService.progressbar.start();
   DataService.getAllFranchiseName().then(function(res) {
     _self.AllFranchiceName = res;
@@ -20,25 +36,29 @@ function AddUserController(mainRef, DataService, MessageService, AuthService,
     toastr.error("Error to load franchiseName")
     MessageService.progressbar.complete();
   });
-  // _self.emailExistError = "";
-  // _self.Roles  = DataService.getRoles();
+
+
   _self.addUser = function(user) {
-    delete user.confirmpassword;
-    user.isActive = true;
     MessageService.progressbar.start();
-    // _self.emailExistError = "";
-    // _self.error = "";
     if (user.confirmpassword != user.password) {
       toastr.error("password does not match");
       MessageService.progressbar.complete();
     } else {
+      angular.forEach(_self.selectedRoles, function(val) {
+        delete val.$$hashKey
+      })
+      delete user.role;
+      user.roles = _self.selectedRoles;
+      delete user.confirmpassword;
       AuthService.signup(user).then(function(res) {
           toastr.success("User created successfully");
           MessageService.progressbar.complete();
+          $state.go("dashboard.user");
+          _self.user = {}
         }, function(err) {
           toastr.error("Error to add User");
           MessageService.progressbar.complete();
-
+          _self.user = {};
         })
         // HttpService.PostApi("/router/signup", user).then(function(res) {
         //
@@ -47,7 +67,7 @@ function AddUserController(mainRef, DataService, MessageService, AuthService,
         //     toastr.error("Email is already exist");
         //   } else {
         //     toastr.success("User added");
-        //     $state.go("dashboard.user");
+        //
         //     _self.user = {};
         //     MessageService.progressbar.complete();
         //
